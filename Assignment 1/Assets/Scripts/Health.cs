ce1eb607f4da72492f616549312ad53c7d5d92eb
Jalenjones015 <jalenjones015@gmail.com>
2021-01-30 13:26:32 -0500

    New Changes

---
+++ Assignment 1/Assets/Scripts/Health.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public Text display;
+     
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assignment 1/Assets/Scripts/Move.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+
+    public float speed;
+    public GameObject Bob;
+    public KeyCode right;
+    public KeyCode left;
+    public KeyCode jump;
+
+   public void Update()
+    {
+        if (Input.GetKey(right))
+        {
+            Bob.transform.Translate(new Vector2(speed * Time.deltaTime, 0));
+        }
+
+        if (Input.GetKey(left))
+        {
+            Bob.transform.Translate(new Vector2(-speed * Time.deltaTime, 0));
+        }
+
+        if (Input.GetKeyDown(jump))
+        {
+            Bob.transform.Translate(new Vector2(0, 2f));
+        }
+    }
+}
