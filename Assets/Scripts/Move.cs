f713e5fded2ac28c2f005b9ad0f9b0f613fadfc7
Jalenjones015 <jalenjones015@gmail.com>
2021-01-31 06:40:45 -0500

    New

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -7,11 +7,18 @@ public class Move : MonoBehaviour
 
     public float speed;
     public GameObject Bob;
+    public Rigidbody rig;
     public KeyCode right;
     public KeyCode left;
     public KeyCode jump;
+    public bool groundcube = true;
+    
 
-   public void Update()
+    public void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+    public void Update()
     {
         if (Input.GetKey(right))
         {
@@ -23,9 +30,17 @@ public class Move : MonoBehaviour
             Bob.transform.Translate(new Vector2(-speed * Time.deltaTime, 0));
         }
 
-        if (Input.GetKeyDown(jump))
+        if (Input.GetKeyDown(jump) && groundcube)
         {
-            Bob.transform.Translate(new Vector2(0, 2f));
+            rig.AddForce(new Vector2(0, 10), ForceMode.Impulse);
+            groundcube = false;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Floor")
+        {
+            groundcube = true;
         }
     }
 }
