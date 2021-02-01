40de4163845806a186005bd80f59afdb6008ce90
Jalenjones015 <jalenjones015@gmail.com>
2021-02-01 02:35:50 -0500

    new
    added menu

---
+++ Assets/Scripts/Main Menu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string startSceneName;
+
+    public void StartPressed()
+    {
+        SceneManager.LoadScene(startSceneName);
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        Debug.Log("Back");
+    }
+}
+
